7ba2efd87a3d6b8eff6c32af305c60fb19dec2f5
Sébastien Dumont <mailme@sebastiendumont.com>
2017-11-24 15:39:08 +0100

    Added WooDependencies and updated package.

---
+++ Gruntfile.js
@@ -0,0 +1,193 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	require('load-grunt-tasks')(grunt);
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			options: {
+				compress: {
+					global_defs: {
+						"EO_SCRIPT_DEBUG": false
+					},
+					dead_code: true
+				},
+				banner: '/*! <%= pkg.title %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
+			},
+			build: {
+				files: [{
+					expand: true, // Enable dynamic expansion.
+					src: ['assets/js/*.js', '!assets/js/*.min.js'], // Actual pattern(s) to match.
+					ext: '.min.js', // Dest filepaths will have this extension.
+				}]
+			}
+		},
+
+		jshint: {
+			options: {
+				reporter: require('jshint-stylish'),
+				globals: {
+					"EO_SCRIPT_DEBUG": false,
+				},
+				'-W099': true, // Mixed spaces and tabs
+				'-W083': true, // Fix functions within loop
+				'-W082': true, // Declarations should not be placed in blocks
+				'-W020': true, // Read only - error when assigning EO_SCRIPT_DEBUG a value.
+			},
+			all: [ 'assets/js/*.js', '!assets/js/*.min.js' ]
+		},
+
+		watch: {
+			scripts: {
+				files: 'assets/js/*.js',
+				tasks: ['jshint', 'uglify'],
+				options: {
+					debounceDelay: 250,
+				},
+			},
+		},
+
+		// Generate .pot file
+		makepot: {
+			target: {
+				options: {
+					domainPath: 'languages', // Where to save the POT file.
+					mainFile: '<%= pkg.name %>.php', // Main project file.
+					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
+					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
+					potHeaders: {
+						'Report-Msgid-Bugs-To': 'https://sebastiendumont.com/contact-me/',
+						'language-team': 'Sébastien Dumont <mailme@sebastiendumont.com>',
+						'language': 'en_US'
+					}
+				}
+			}
+		},
+
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>', // Project text domain.
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'*.php',
+					'**/*.php', // Include all files
+					'!woo-dependencies/**', // Exclude woo-dependencies/
+					'!node_modules/**', // Exclude node_modules/
+					'!tmp/**', // Exclude tmp/
+				],
+				expand: true
+			},
+		},
+
+		potomo: {
+			dist: {
+				options: {
+					poDel: false
+				},
+				files: [{
+					expand: true,
+					cwd: 'languages',
+					src: ['*.po'],
+					dest: 'languages',
+					ext: '.mo',
+					nonull: false
+				}]
+			}
+		},
+
+		// Bump version numbers (replace with version in package.json)
+		replace: {
+			Version: {
+				src: [
+					'readme.txt',
+					'<%= pkg.name %>.php'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /Stable tag:.*$/m,
+						to: "Stable tag: <%= pkg.version %>"
+					},
+					{
+						from: /Version:.*$/m,
+						to: "Version:     <%= pkg.version %>"
+					},
+					{
+						from: /public \$version = \'.*.'/m,
+						to: "public $version = '<%= pkg.version %>'"
+					},
+					{
+						from: /public \$version      = \'.*.'/m,
+						to: "public $version      = '<%= pkg.version %>'"
+					}
+				]
+			}
+		},
+
+		// Copies the plugin to create deployable plugin.
+		copy: {
+			deploy: {
+				src: [
+					'**',
+					'!.*',
+					'!*.md',
+					'!.*/**',
+					'.htaccess',
+					'!Gruntfile.js',
+					'!package.json',
+					'!node_modules/**',
+					'!.DS_Store',
+					'!npm-debug.log'
+				],
+				dest: '<%= pkg.name %>',
+				expand: true,
+				dot: true
+			}
+		},
+
+		// Compresses the deployable plugin folder.
+		compress: {
+			zip: {
+				options: {
+					archive: './<%= pkg.name %>-v<%= pkg.version %>.zip',
+					mode: 'zip'
+				},
+				files: [
+					{
+						src: './<%= pkg.name %>/**'
+					}
+				]
+			}
+		},
+
+		// Deletes the deployable plugin folder once zipped up.
+		clean: [ '<%= pkg.name %>' ]
+
+	});
+
+	grunt.registerTask( 'test', [ 'jshint', 'newer:uglify' ] );
+	grunt.registerTask( 'dev', [ 'replace', 'newer:uglify', 'makepot' ] );
+	grunt.registerTask( 'build', [ 'replace', 'newer:uglify', 'checktextdomain', 'makepot' ] );
+	grunt.registerTask( 'update-pot', [ 'checktextdomain', 'makepot' ]);
+	grunt.registerTask( 'zip', [ 'copy', 'compress', 'clean' ]);
+};
